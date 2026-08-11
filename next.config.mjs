3726be9be16e58f93d7bec2945723e1a905a39dc
rajat goyal <104815417+Rajat-goyal-ux@users.noreply.github.com>
2026-08-11 22:07:36 +0530

    Configure automatic 308 permanent redirect routing all long Vercel URLs directly to short domain pva1500pro.vercel.app

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'pva1500-pro-(.*)\\.vercel\\.app',
+          },
+        ],
+        destination: 'https://pva1500pro.vercel.app/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
